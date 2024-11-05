95ce258ce47498942b9f0dd8c738e9234334ed92
Eric S <estriler@hotmail.com>
2024-11-04 19:59:32 -0600

    completed task 1 of graded assignment

--- queries.sql
+++ queries.sql
@@ -1,4 +1,12 @@
 --Part 1
+CREATE TABLE job (
+    id INT,
+    name VARCHAR(255),
+    employer VARCHAR(255),
+    location VARCHAR(255),
+    position_type VARCHAR(255),
+    skill VARCHAR(255)
+);
 
 --Part 2
 
