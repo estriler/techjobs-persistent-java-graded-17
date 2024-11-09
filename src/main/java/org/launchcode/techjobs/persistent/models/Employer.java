3336cb0f4edd6f338e388c790dacfebc777e9dfe
Eric S <estriler@hotmail.com>
2024-11-09 17:41:52 -0600

    added jobs field to Employer

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,13 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -21,4 +26,8 @@ public class Employer extends AbstractEntity {
     public void setLocation(@NotBlank @Size(min = 1, max = 50) String location) {
         this.location = location;
     }
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
 }
