9d088fa64a58a3e1a8ccdaa33fca06edcd06bc2d
Eric S <estriler@hotmail.com>
2024-11-09 20:04:08 -0600

    added job field in Skill class with a ManyToMany relationship

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,13 +1,20 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
     @Size(max = 200)
     public String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private final List<Job> jobs = new ArrayList<>();
 
     //no-arg constructor
     public Skill(){}
@@ -20,4 +27,9 @@ public class Skill extends AbstractEntity {
     public void setDescription(@Size(max = 200) String description) {
         this.description = description;
     }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
 }
